3f534bf28b080a9320edfb24c512fff20320e2e2
AbsoluteChan <363155763@qq.com>
2017-05-24 17:41:38 +0800

    第八章
    1.添加了标题+按钮+checkbox
    2.显示都正常。

--- src/com/absolute/criminalintent/Crime.java
+++ src/com/absolute/criminalintent/Crime.java
@@ -1,14 +1,20 @@
 package com.absolute.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	
 	private UUID mId;
 	private String mTitle;
+	//添加2个变量
+	private Date mDate;
+	private boolean mSolved;
+	
 	
 	public Crime(){
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	/**
@@ -30,6 +36,34 @@ public class Crime {
 	 */
 	public UUID getId() {
 		return mId;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return mDate;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	/**
+	 * @return the solved
+	 */
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	/**
+	 * @param solved the solved to set
+	 */
+	public void setSolved(boolean solved) {
+		mSolved = solved;
 	}
 	
 	
--- src/com/absolute/criminalintent/CrimeFragment.java
+++ src/com/absolute/criminalintent/CrimeFragment.java
@@ -7,15 +7,22 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 
 	/* (non-Javadoc)
 	 * @see android.support.v4.app.Fragment#onCreate(android.os.Bundle)
@@ -55,6 +62,21 @@ public class CrimeFragment extends Fragment {
 			public void afterTextChanged(Editable c) {
 				// TODO Auto-generated method stub
 				
+			}
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		String date = (String)DateFormat.format("EE, MMMM,dd»’,yyyy",mCrime.getDate());
+		mDateButton.setText(date);
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				mCrime.setSolved(isChecked);
 			}
 		});
 		
