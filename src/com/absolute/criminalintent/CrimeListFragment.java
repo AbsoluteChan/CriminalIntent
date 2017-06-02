7e8b7df7e611bc975a4c19caddc32babd0fa0b0f
AbsoluteChan <363155763@qq.com>
2017-06-02 12:04:06 +0800

    第九章.5 自定义 adapter子类
    定制布局并显示

--- src/com/absolute/criminalintent/CrimeListFragment.java
+++ src/com/absolute/criminalintent/CrimeListFragment.java
@@ -1,13 +1,18 @@
 package com.absolute.criminalintent;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 
@@ -24,8 +29,9 @@ public class CrimeListFragment extends ListFragment {
 		getActivity().setTitle(R.string.crime_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 		
-		ArrayAdapter<Crime> adapter = 
-				new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
+		/*ArrayAdapter<Crime> adapter = 
+				new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);*/
+		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
 		
 		
@@ -38,8 +44,52 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		// TODO Auto-generated method stub
-		Crime c = (Crime)(getListAdapter()).getItem(position);
+		/*Crime c = (Crime)(getListAdapter()).getItem(position);*/
+		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 		Log.d(TAG, c.getTitle() + "was clicked");
+		
+		
+	}
+	
+	private class CrimeAdapter extends ArrayAdapter<Crime>{
+
+		public CrimeAdapter(ArrayList<Crime> crimes)
+		{
+			super(getActivity(),0,crimes);
+		}
+
+		/* (non-Javadoc)
+		 * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
+		 */
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			//if we weren't given a view inflate one
+			if(convertView == null){
+				convertView = getActivity().getLayoutInflater()
+						.inflate(R.layout.list_item_crime, null);
+			}
+			//configure the view for this Crime
+			
+			Crime c = getItem(position);
+			
+			TextView titileTextView = 
+					(TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titileTextView.setText(c.getTitle());
+			
+			TextView dateTextView = 
+					(TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText( c.getDate().toString());
+			
+			CheckBox solvedCheckBox = 
+					(CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.isSolved());
+						
+			
+			return convertView;
+		}
+		
+		
 		
 		
 	}
